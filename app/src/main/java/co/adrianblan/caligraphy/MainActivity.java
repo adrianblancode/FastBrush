6eb5f606b6b91a5448a3431e85ebf4ac238a65b6
adrianblp <adrianblp@gmail.com>
2016-01-11 22:21:16 +0100

    First commit

---
+++ app/src/main/java/co/adrianblan/caligraphy/MainActivity.java
@@ -0,0 +1,36 @@
+package co.adrianblan.caligraphy;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyGLSurfaceView glSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        glSurfaceView = new MyGLSurfaceView(this);
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+}
